a0f1273b66680f66e2cdc856847fa1e7ac1de511
Ferrari Lee <shiyung@gmail.com>
2016-04-02 02:56:36 +0800

    support basci send message function

--- index.js
+++ index.js
@@ -2,22 +2,50 @@
 
 const _ = require('lodash');
 const got = require('got');
+const debug = require('debug')('slack-helper');
 
-function slackHelper(opts) {
+const SLACK_API = 'https://slack.com/api/';
+
+/**
+ * slack-helper class
+ *
+ * @param String Slack API Token
+ */
+function slackHelper(token) {
   if (!(this instanceof slackHelper)) {
-    return new slackHelper(opts);
+    return new slackHelper(token);
   }
 
-  if (!_.has(opts, 'url')) {
-    throw new Error('Need slack webhook url');
-  } else if (!_.has(opts, 'token')) {
-    throw new Error('Need slack api token');
+  if (!_.isString(token)) {
+    throw new Error('Need an valid slack api token');
   }
+
+  this.token = token;
 }
 
 slackHelper.prototype = {
-  send: function(message) {
+  send: function(channel, message, opts) {
+    let self = this;
+    let reqUrl = `${SLACK_API}/chat.postMessage`;
 
+    return new Promise(function(resolve, reject) {
+      got.post(reqUrl, {
+        json: true,
+        body: {
+          token : self.token,
+          channel: channel,
+          text: message
+        }
+      }).then((resp) => {
+        let result = resp.body;
+
+        return (result.ok !== true)
+          ? reject(new Error(result.error))
+          : resolve(result);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
   }
 };
 
