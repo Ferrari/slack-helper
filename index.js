52340afffa04b75e6fe86d4e4c5dabc136979141
Ferrari Lee <shiyung@gmail.com>
2016-04-04 05:21:50 +0800

    add getUserList functions

--- index.js
+++ index.js
@@ -23,11 +23,32 @@ function slackHelper(token) {
 }
 
 slackHelper.prototype = {
+  getUserList: function() {
+    let self = this;
+    let reqUrl = `${SLACK_API}/users.list`;
+
+    return new Promise(function (resolve, reject) {
+      got.get(reqUrl, {
+        json: true,
+        query: {
+          token: self.token
+        }
+      }).then(resp => {
+        let result = resp.body;
+
+        return (result.ok !== true)
+          ? reject(new Error(result.error))
+          : resolve(result);
+      }).catch(error => {
+        reject(error);
+      });
+    });
+  },
   send: function(channel, message, opts) {
     let self = this;
     let reqUrl = `${SLACK_API}/chat.postMessage`;
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       got.post(reqUrl, {
         json: true,
         body: {
@@ -45,6 +66,8 @@ slackHelper.prototype = {
         reject(error);
       });
     });
+  },
+  sendToUsers: function(users, channel, message, opts) {
   }
 };
 
